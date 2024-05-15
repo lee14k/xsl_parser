adf08f3ac92ae446a8e4ea84e597f808dfca36e8
lee14k <kmkovach@mtu.edu>
2024-05-15 00:25:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# xsl_parser
---
+++ xslparse.py
@@ -0,0 +1,7 @@
+import pandas
+
+excel_data_fragment = pandas.read_excel('inventory_list_7.xlsx', sheet_name='Annuals')
+
+json_str = excel_data_fragment.to_json()
+
+print('Excel Sheet to JSON:\n', json_str)
